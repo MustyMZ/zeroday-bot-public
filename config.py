3d8ff9b51373df64b710d1f834dd8aacb5a49495
MustyMZ <mstfa_zrlu@hotmail.com>
2025-05-01 21:08:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
-import os
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
+# Telegram
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+# Binance
 BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
-BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET")
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+BINANCE_SECRET_KEY = os.getenv("BINANCE_SECRET_KEY")
+
+# OpenAI
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# Etherscan
+ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY")
