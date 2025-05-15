08e94d9ade9b933f5eb515eb259e32eeaef828f4
MustyMZ <mstfa_zrlu@hotmail.com>
2025-05-15 11:15:33 +0300

    Update pro_trader_smart.py

--- pro_trader_smart.py
+++ pro_trader_smart.py
@@ -12,7 +12,7 @@ def send_telegram_message(message):
     try:
         token = os.getenv("TELEGRAM_BOT_TOKEN")
         chat_id = os.getenv("TELEGRAM_CHAT_ID")
-        url = f"https://api.telegram.org/bot{token}/sendMessage"
+        url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
         payload = {
             "chat_id": chat_id,
             "text": message
